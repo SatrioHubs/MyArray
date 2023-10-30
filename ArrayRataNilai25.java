4c3c398c9c4a1f88f2cfbd11c3abe6f166051bdc
SatrioHubs <satrioramadhani387@gmail.com>
2023-10-30 21:45:40 +0700

    hitung rata rata nilai tidak lulus

--- ArrayRataNilai25.java
+++ ArrayRataNilai25.java
@@ -4,23 +4,30 @@ public class ArrayRataNilai25 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
+        System.out.print("Masukan Jumlah Mahasiswa: ");
+        int jumMhs = input.nextInt();
+
+        int[] nilaiMhs = new int[jumMhs];
         double total = 0;
-        double rata2;   
+        double rata2 = 0, rata2_tidakLulus = 0 ;   
         int countLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukan Nilai Mahasiswa ke-"+i+" : ");
+            System.out.print("Masukan Nilai Mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = input.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
             if (nilaiMhs[i] > 70) {
                 countLulus++;
+            }else{
+                rata2_tidakLulus += nilaiMhs[i];
             }
             total += nilaiMhs[i];
         }
         rata2 = total/nilaiMhs.length;
+        rata2_tidakLulus /= (nilaiMhs.length - countLulus); 
         System.out.println("Rata-rata = "+rata2);
-        System.out.println("Mahasiswa yang lulus : "+countLulus);
+        System.out.println("Rata-rata tidak lulus: "+rata2_tidakLulus);
+        input.close();
     }
 }
