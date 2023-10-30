38c737992246edb507ffdc922ce67b7f51b2b492
SatrioHubs <satrioramadhani387@gmail.com>
2023-10-30 10:10:01 +0700

    tugas

--- ArrayNilai25.java
+++ ArrayNilai25.java
@@ -5,13 +5,18 @@ public class ArrayNilai25 {
         Scanner input = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukan Nilai Akhir ke-"+i+" : ");
             nilaiAkhir[i] = input.nextInt();
 
         }
         for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah " + nilaiAkhir[i]);
+           if (nilaiAkhir[i] > 70) {
+            System.out.println("Mahasiswa ke-"+i+" Lulus");
+           } else{
+            System.out.println("Mahasiswa ke-"+i+" Tidak Lulus");
+           }
+            
         }
     }
 }
