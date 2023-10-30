72ac34049e46970c21da3240ae4d8a721f9927fe
SatrioHubs <satrioramadhani387@gmail.com>
2023-10-30 21:02:47 +0700

    hitung lulus

--- ArrayRataNilai25.java
+++ ArrayRataNilai25.java
@@ -7,15 +7,20 @@ public class ArrayRataNilai25 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;   
+        int countLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukan Nilai Mahasiswa ke-"+i+" : ");
             nilaiMhs[i] = input.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > 70) {
+                countLulus++;
+            }
             total += nilaiMhs[i];
         }
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata = "+rata2);
+        System.out.println("Mahasiswa yang lulus : "+countLulus);
     }
 }
