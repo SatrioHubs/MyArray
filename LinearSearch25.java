aa1670f4d6900226d0dfdc5a5606de81d9efdcb1
SatrioHubs <satrioramadhani387@gmail.com>
2023-10-31 11:19:43 +0700

    key tidak di temukan

--- LinearSearch25.java
+++ LinearSearch25.java
@@ -1,17 +1,35 @@
+import java.util.Scanner;
+
 public class LinearSearch25 {
 
     public static void main(String[] args) {
-        
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Masukan panjang Array: ");
+        int panjang = input.nextInt();
+
+        int[] arrayInt = new int [panjang];
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukan element array ke-"+i+" : ");
+            arrayInt[i] = input.nextInt();
+        }
+        System.out.print("Masukan Key yang di cari: ");
+        int key = input.nextInt();
+        int hasil = 0;
+        boolean keytemu = false;
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
+                keytemu = true;
                 break;
             }
         }
-        System.out.println("key ada dalama array pada posisi indeks ke-"+ hasil);
+        if (keytemu) {
+            System.out.println("Key yang anda cari ada pada element ke-"+hasil);
+        } else{
+            System.out.println("Key tidak di temukan");
+        }
+        input.close();
     }
 }
