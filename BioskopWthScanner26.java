8cff6c26d64e769fd6563d26301172d812813684
SatrioHubs <satrioramadhani387@gmail.com>
2023-11-05 22:10:21 +0700

    Pertanyaan full

--- BioskopWthScanner26.java
+++ BioskopWthScanner26.java
@@ -7,8 +7,18 @@ public class BioskopWthScanner26 {
         int baris, kolom;
         String nama, next;
         String[][] penonton = new String[4][2];
+        boolean menu=true;
+        while (menu!=false) {
+            System.out.println("Pilih menu");
+            System.out.println("1. input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            int option=sc.nextInt();
+            sc.nextLine();
 
-        while (true) {
+            switch (option) {
+                case 1:
+            while (true) {
             System.out.print("Masukan nama: ");
             nama = sc.nextLine();
             System.out.print("Masukan baris: ");
@@ -16,6 +26,18 @@ public class BioskopWthScanner26 {
             System.out.print("Masikan kolom: ");
             kolom = sc.nextInt();
             sc.nextLine();
+
+            if(baris==5||kolom==3){
+                System.out.println("Baris atau kolom tidak tersedia");
+                continue;
+            }
+
+            if(penonton[baris-1][kolom-1]!=null){
+                System.out.println("tempat sudah terisi pilih tempat lain!");
+                continue;
+            }else{
+            penonton[baris-1][kolom-1] = nama;
+            }
 
             penonton[baris -1][kolom -1] = nama;
 
@@ -25,6 +47,31 @@ public class BioskopWthScanner26 {
             if (next.equalsIgnoreCase("n")) {
                 break;
             }
+        }      
+              break;
+
+            case 2:
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 2; j++) {
+                    if (penonton[i][j]==null) {
+                        penonton[i][j]="***";
+                    }
+                }
+            }
+
+            for (int i = 0; i < penonton.length; i++) {
+                System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
+            }
+            break;
+
+            case 3:
+            System.out.println("Exit");
+            menu = false;
+            break;
+                default:
+                System.out.println("Wrong input");    
+        }
+        
         }
     }
 }
